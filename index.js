c61d161e6ee39cbccf9ec7b4495258e4903a6744
Keve1227 <killenlillen02@gmail.com>
2019-10-27 22:36:25 +0100

    Created the function "composeWord".

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 const dictionary = require("./dictionary.json");
+
+let startsWithVowel = function (str) {
+    if (!startsWithVowel || startsWithVowel.length < 1) return true;
+
+    return str.match(/.*[aeiou]$/i);
+}
+
+exports.composeWord = function (root, suffix, prefix) {
+    if (startsWithVowel(suffix)) {
+        root += 'o';
+    }
+    
+    if (root[root.length - 1] === suffix[0]) {
+        suffix = suffix.substr(1);
+    }
+
+    if (prefix && prefix[prefix.length - 1] === root[0]) {
+        prefix += '-';
+    }
+
+    return (prefix ? prefix : "") + root + suffix;
+}
